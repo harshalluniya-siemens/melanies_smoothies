a97e386a9c59c2c6ebfd4ed31ed2ba718f4f6e10
harshalluniya-siemens <harshal.luniya@siemens.com>
2026-09-24 19:16:54 +0530

    update streamlit_app.py
    Corrected line 49 with a '('

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,5 +46,5 @@ if ingredients_list:
             st.success('Your Smoothie is ordered!', icon="✅")
 #New section to display smoothiefront nutrional information
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("(https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
 st.text(smoothiefroot_response.json())        
