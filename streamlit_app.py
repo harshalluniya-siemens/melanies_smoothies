601b049271c3b64841a6eb6d294bff5a984caaf6
harshalluniya-siemens <harshal.luniya@siemens.com>
2026-09-24 19:11:53 +0530

    Update streamlit_app.py
    Added line 47 to 50

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,5 +44,8 @@ if ingredients_list:
             session.sql(my_insert_stmt).collect() 
             
             st.success('Your Smoothie is ordered!', icon="✅")
-
+#New section to display smoothiefront nutrional information
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
         
